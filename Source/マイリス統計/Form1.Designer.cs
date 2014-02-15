790576556dbe7e6747766032bd70cbc5567e5b7a
Surigoma <admin@mail.surigoma.net>
2014-02-16 01:39:17 +0900

    First commit
    Source Upload.

---
+++ Source/マイリス統計/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace マイリス統計
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Title = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Time = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Link = new System.Windows.Forms.DataGridViewLinkColumn();
+            this.guid = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pubDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.description = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(68, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 19);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "マイリスID";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(174, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "GO";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Title,
+            this.Time,
+            this.Link,
+            this.guid,
+            this.pubDate,
+            this.description});
+            this.dataGridView1.EnableHeadersVisualStyles = false;
+            this.dataGridView1.Location = new System.Drawing.Point(14, 33);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowTemplate.Height = 21;
+            this.dataGridView1.Size = new System.Drawing.Size(781, 239);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Title
+            // 
+            this.Title.HeaderText = "Title";
+            this.Title.Name = "Title";
+            this.Title.ReadOnly = true;
+            // 
+            // Time
+            // 
+            this.Time.HeaderText = "Time";
+            this.Time.Name = "Time";
+            this.Time.ReadOnly = true;
+            // 
+            // Link
+            // 
+            this.Link.HeaderText = "Link";
+            this.Link.Name = "Link";
+            this.Link.ReadOnly = true;
+            // 
+            // guid
+            // 
+            this.guid.HeaderText = "guid";
+            this.guid.Name = "guid";
+            this.guid.ReadOnly = true;
+            // 
+            // pubDate
+            // 
+            this.pubDate.HeaderText = "pubDate";
+            this.pubDate.Name = "pubDate";
+            this.pubDate.ReadOnly = true;
+            // 
+            // description
+            // 
+            this.description.HeaderText = "内容";
+            this.description.Name = "description";
+            this.description.ReadOnly = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(255, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "総再生時間：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(332, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "00:00:00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(807, 284);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Title;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Time;
+        private System.Windows.Forms.DataGridViewLinkColumn Link;
+        private System.Windows.Forms.DataGridViewTextBoxColumn guid;
+        private System.Windows.Forms.DataGridViewTextBoxColumn pubDate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn description;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Source/マイリス統計/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Xml;
+
+namespace マイリス統計
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] formats = {
+                @"mm\:ss",
+                @"m\:ss",
+                @"hhh\:mm\:ss",
+                @"hh\:mm\:ss",
+                @"h\:mm\:ss",
+            };
+            WebClient wc = new WebClient();
+            wc.Encoding = Encoding.UTF8;
+            string rowxml = wc.DownloadString("http://www.nicovideo.jp/mylist/" + int.Parse(textBox1.Text) + "?rss=2.0");
+            rowxml = rowxml.Replace("<![CDATA[", "").Replace("]]>", "").Replace("&", "%26");
+            XmlDocument xd = new XmlDocument();
+            xd.LoadXml(rowxml);
+            var items = xd["rss"]["channel"];
+            dataGridView1.Rows.Clear();
+            var alltime = new TimeSpan();
+            XmlNodeList xnl = items.GetElementsByTagName("item");
+            var culture = System.Globalization.CultureInfo.CurrentCulture;
+            foreach (XmlNode node in xnl)
+            {
+                int index = dataGridView1.Rows.Add();
+                dataGridView1.Rows[index].Cells["Title"].Value = node["title"].InnerText;
+                XmlNode strong = node.SelectNodes("child::description/p[last()]")[0]["small"]["strong"];
+                dataGridView1.Rows[index].Cells["Time"].Value = strong.InnerText;
+                var ts = TimeSpan.ParseExact(strong.InnerText, formats, culture);
+                alltime = alltime.Add(ts);
+                dataGridView1.Rows[index].Cells["Link"].Value = node["link"].InnerText;
+                dataGridView1.Rows[index].Cells["guid"].Value = node["guid"].InnerText;
+                dataGridView1.Rows[index].Cells["pubDate"].Value = node["pubDate"].InnerText;
+                dataGridView1.Rows[index].Cells["description"].Value = node["description"].InnerText;
+            }
+            label3.Text = alltime.ToString(@"hh\:mm\:ss");
+        }
+    }
+}
